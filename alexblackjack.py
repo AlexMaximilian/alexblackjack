99c596a9b617d8175e5f34b4a003eb7c411fb5c4
Alex Maximilian <alexandervanpoecke1@gmail.com>
2021-11-04 18:18:58 +0000

    Added the deal the cards system

--- alexblackjack.py
+++ alexblackjack.py
@@ -17,6 +17,22 @@ losses = 0
 #Add money system associated with wins and losses
 balance = 1000
 
+def thedeal(deck):
+    hand = []
+    for i in range(2):
+        random.shuffle(deck)
+        card = deck.pop()
+        if card == 11:card = "J"
+        if card == 12:card = "Q"
+        if card == 13:card = "K"
+        if card == 14:card = "A"
+        hand.append(card) #Took the card from the deck removed it with pop and added it to hand
+    return hand
+
+print(thedeal(deck))
+print(deck)
+
+
 
 
 
