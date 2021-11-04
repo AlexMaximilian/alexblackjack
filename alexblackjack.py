0b3d0496ed68f7cc252633d0467f382ebe6e9cb4
Alex Maximilian <alexandervanpoecke1@gmail.com>
2021-11-04 17:57:59 +0000

    First commit

---
+++ alexblackjack.py
@@ -0,0 +1,16 @@
+#Alex Maximilian github.com/Alexmaximillian
+#Simple Blackjack game inspired by other repos
+#Enjoy
+
+import os
+import random
+
+decks = input("Enter the number of decks you want to play with: ")
+
+#Sets up the  cards
+deck = [2,3,4,5,6,7,8,9,10,11,12,13,14]*(int(decks)*4)
+
+
+
+
+
